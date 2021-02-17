c60a7841038acdaabe4c160ca14f6997c3efb789
HelpMyanmar <helpmyanmarcitizens@gmail.com>
2021-02-17 15:36:39 +0630

    1 file modified

--- src/component/donate_home.js
+++ src/component/donate_home.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 function Donate_Home() {
-  return <Card style={{ height: "200px" }} />;
+  return (
+    <Card style={{ height: "200px" }}>
+      <p>Hello</p>
+    </Card>
+  );
 }
 
 export default Donate_Home;
