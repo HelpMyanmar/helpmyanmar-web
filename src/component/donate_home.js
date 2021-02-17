22875687fa3fc5bb1fab98e206c4c5b3db0fb068
HelpMyanmar <helpmyanmarcitizens@gmail.com>
2021-02-17 15:33:31 +0630

    2 files modified

---
+++ src/component/donate_home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Donate_Home() {
+  return <Card style={{ height: "200px" }} />;
+}
+
+export default Donate_Home;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import Donate_Home from "../component/donate_home";
 
 function Home_Page() {
   return (
     <Container>
       <Row>
-        <Col md={3} style={{backgroundColor:"red"}}>1</Col>
-        <Col md={6} style={{backgroundColor:"yellow"}}>1</Col>
-        <Col md={3} style={{backgroundColor:"green"}}>1</Col>
+        <Col md={3} style={{ marginTop: "20px" }}>
+          <Donate_Home />
+        </Col>
+        <Col md={6} style={{ backgroundColor: "yellow", marginTop: "20px" }}>
+          1
+        </Col>
+        <Col md={3} style={{ backgroundColor: "green", marginTop: "20px" }}>
+          1
+        </Col>
       </Row>
     </Container>
   );
